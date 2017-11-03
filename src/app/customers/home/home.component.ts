f7c9e0044ec88205a78a07b03bc11dbde0aa0990
jeffansay <jeffansay21@gmail.com>
2017-11-03 14:55:07 +0800

    Adding map to the home in customer tab

--- src/app/customers/home/home.component.ts
+++ src/app/customers/home/home.component.ts
@@ -1,9 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, NgZone, ViewChild} from '@angular/core';
+import { AgmCoreModule } from '@agm/core';
+import { } from 'googlemaps';
+import { MapsAPILoader } from '@agm/core';
 //import { DataService } from '../../services/data.service';
 //import { Http } from '@angular/http';
 import { HttpClient, HttpParams , HttpHeaders } from '@angular/common/http';
 import { Post } from '../../services/post';
 import { Observable } from 'rxjs/Observable';
+
+//test begin
+
+
+
+
+//test end
 
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
@@ -19,12 +29,72 @@ export class HomeComponent implements OnInit {
   posts: Observable<any>;
   newPost: Observable<any>;
   readonly  Rootkk = 'https://jsonplaceholder.typicode.com';
-  constructor(private http:HttpClient) { }
+
+  //test here
+  lat: number;
+  lng: number;
+  zoom: number;
+
+  //begin here
+  constructor(private http:HttpClient,private mapsAPILoader: MapsAPILoader,
+  private ngZone: NgZone) { }
+
+
+  @ViewChild("search")
+  public searchElementRef: ElementRef;
 
   ngOnInit() {
     //console.log(this.dataSrv.urlget('localhost'));
 
+    this.setCurrentPosition();
+
+    this.mapsAPILoader.load().then(() => {
+
+      let autocomplete = new google.maps.places.Autocomplete(this.searchElementRef.nativeElement, {
+        types: ["address"],
+      });
+
+
+    // Bias the SearchBox results towards current map's viewport.
+  autocomplete.addListener('bounds_changed', function() {
+    this.searchElementRef.nativeElement.setBounds(autocomplete.getBounds());
+  });
+
+
+      autocomplete.addListener("place_changed", () => {
+        this.ngZone.run(() => {
+          //get the place result
+          let place: google.maps.places.PlaceResult = autocomplete.getPlace();
+
+          //verify result
+          if (place.geometry === undefined || place.geometry === null) {
+            return;
+          }
+
+          //set latitude, longitude and zoom
+          this.lat = place.geometry.location.lat();
+          this.lng = place.geometry.location.lng();
+           this.zoom = 17;
+        });
+      });
+
+    });
+
+
   }
+
+
+  private setCurrentPosition() {
+    /// set user's location
+    if ("geolocation" in navigator) {
+   navigator.geolocation.getCurrentPosition((position) => {
+     this.lat = position.coords.latitude;
+     this.lng = position.coords.longitude;
+     this.zoom = 12;
+   });
+  }
+  }
+  
   getPost(uidt){
       console.log('test button clicked' + uidt);
       let params = new HttpParams().set('userId',uidt);
