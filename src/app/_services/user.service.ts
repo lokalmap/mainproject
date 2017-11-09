d3ba8514aad763b5837f6f0615e441650dcc9140
Hydie <corneliohydie@gmail.com>
2017-11-09 19:04:38 +0800

    changes : user.service.ts file, added dropdown for accountType (Customer and Provider values)

--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -8,22 +8,22 @@ export class UserService {
     constructor(private http: Http) { }
 
     getAll() {
-        return this.http.get('/lokalusers').map((response: Response) => response.json());
+        return this.http.get('/Users').map((response: Response) => response.json());
     }
 
     getById(_id: string) {
-        return this.http.get('/lokalusers/' + _id).map((response: Response) => response.json());
+        return this.http.get('/Users/' + _id).map((response: Response) => response.json());
     }
 
     create(user: User) {
-        return this.http.post('/lokalusers', user);
+        return this.http.post('/Users', user);
     }
 
     update(user: User) {
-        return this.http.put('/lokalusers/' + user._id, user);
+        return this.http.put('/Users/' + user._id, user);
     }
 
     delete(_id: string) {
-        return this.http.delete('/lokalusers/' + _id);
+        return this.http.delete('/Users/' + _id);
     }
 }
